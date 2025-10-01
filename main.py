0be8d54f7b41ac295d928decccfb91ded6bc7907
gramtop961 <gramtop961@gmail.com>
2025-10-02 03:28:04 +0530

    Add main.py from template

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI Backend!"}
+
+@app.get("/api/hello")
+def hello():
+    return {"message": "Hello from the backend API!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
